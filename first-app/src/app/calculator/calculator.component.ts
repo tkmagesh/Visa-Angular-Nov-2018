dd62ce0134c78403b6681c2d6e7999996b67df8f
Magesh <tkmagesh77@gmail.com>
2018-11-29 15:28:42 +0000

    condition add/remove css classes

--- first-app/src/app/calculator/calculator.component.ts
+++ first-app/src/app/calculator/calculator.component.ts
@@ -3,7 +3,8 @@ import { CalculatorModel } from './calculatorModel';
 
 @Component({
 	selector : 'app-calculator',
-	templateUrl : 'calculator.component.html'
+	templateUrl : 'calculator.component.html',
+	styleUrls : ['calculator.component.css']
 })
 export class CalculatorComponent{
 	model : CalculatorModel = new CalculatorModel();
